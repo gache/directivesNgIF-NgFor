b9d5940de7fa1b1a63e3784fdb1bf14ca771e07a
Gache <erickfrancodelgado@hotmail.com>
2020-01-17 13:05:44 +0100

    Creation du body

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
+    HeaderComponent,
+    BodyComponent,
 
   ],
   imports: [
