ddc84d0688a270c15e91d45a0919dc088979853b
Gache <erickfrancodelgado@hotmail.com>
2020-01-17 14:41:29 +0100

    creation de la dir NgIf

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BodyComponent implements OnInit {
 
+
+  // Creation de mon Objet
+  phrase: any ={
+    autor: "spider-man",
+    bio: "Peter Parker, alias Spider-Man (souvent écrit « Spiderman » de façon erronée), est un super-héros évoluant dans l'univers Marvel de la maison d'édition Marvel Comics. Créé par le scénariste Stan Lee et le dessinateur Steve Ditko, le personnage de fiction apparaît pour la première fois dans le comic book Amazing Fantasy"
+  };
+
   constructor() { }
 
   ngOnInit() {
